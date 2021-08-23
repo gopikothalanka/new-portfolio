5f212b717a05ee382025be559f6f9b731becd0cd
Gopi <mail2gvrm@gmail.com>
2021-08-23 14:19:16 +0530

    cv is Updated

--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -5,7 +5,7 @@ export default function home() {
     <div className="Hero">
       <h4>Hello, This is</h4>
       <h1> Gopi Kothalanka</h1>
-      <p>I am an aspring Full stack developer from Rajahmundry, India. I create professional websites. I love art and always try to show unique views to the audience through my design.</p>
+      <p>I am an aspiring Full stack developer from Rajahmundry, India. I create professional websites. I love art and always try to show unique views to the audience through my design.</p>
     </div>
   )
 }
